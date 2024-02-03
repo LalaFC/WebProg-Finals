3102de718ba74845f349fe4eb2a5286d0735d4ba
Angelik Operio <122664874+LalaFC@users.noreply.github.com>
2024-02-03 23:27:49 +0800

    Initial Commit

---
+++ wp-finals.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on("click", function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top,
+                },
+                800,
+                function () {
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                }
+            );
+        } // End if
+    });
+});
+
+$(document).ready(function(){
+    $(".col-10 section").each(function(e) {
+        if (e != 0)
+            $(this).hide();
+    });
+    $("#aboutme").click(function(){
+        if ($(".col-10 section:visible").next().length != 0)
+            $(".col-10 section:visible").fadeOut(function(){
+                $(this).next().fadeIn();
+            });
+        else {
+            $(".col-10 section:visible").fadeOut(function(){
+                $(".col-10 section:first").fadeIn();
+            });
+        }
+        return false;
+    });
+    $("#product-menu").click(function(){
+        if ($(".col-10 section:visible").prev().length != 0)
+            $(".col-10 section:visible").fadeOut(function(){
+                $(this).prev().fadeIn();
+            });
+        else {
+            $(".col-10 section:visible").fadeOut(function(){
+                $(".col-10 section:last").fadeIn();
+            });
+        }
+        return false;
+    });
+});
